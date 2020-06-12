418bb6af3c0f396f0375d155013820bb7e132816
BuruTang <nktbj1511203@163.com>
2020-06-12 15:10:53 +0800

    assign3 original

---
+++ Code/build/CMakeFiles/Rasterizer.dir/DependInfo.cmake
@@ -0,0 +1,48 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/ComputerGraphics/Assignment3/Code/Texture.cpp" "E:/ComputerGraphics/Assignment3/Code/build/CMakeFiles/Rasterizer.dir/Texture.cpp.obj"
+  "E:/ComputerGraphics/Assignment3/Code/Triangle.cpp" "E:/ComputerGraphics/Assignment3/Code/build/CMakeFiles/Rasterizer.dir/Triangle.cpp.obj"
+  "E:/ComputerGraphics/Assignment3/Code/main.cpp" "E:/ComputerGraphics/Assignment3/Code/build/CMakeFiles/Rasterizer.dir/main.cpp.obj"
+  "E:/ComputerGraphics/Assignment3/Code/rasterizer.cpp" "E:/ComputerGraphics/Assignment3/Code/build/CMakeFiles/Rasterizer.dir/rasterizer.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  ".././include"
+  "E:/ComputerGraphics"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/build/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/build/x64/MinGW"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/include/opencv"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/core/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/flann/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/imgproc/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/ml/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/photo/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/video/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/dnn/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/features2d/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/imgcodecs/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/shape/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/videoio/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/calib3d/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/highgui/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/objdetect/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/stitching/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/superres/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/ts/include"
+  "D:/OpenCV/opencv-3.4.10-vc14_vc15/opencv/sources/modules/videostab/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
